a05bb8343cf9e772f30434321d058194644a8806
Leonardo Zavala-Jimenez <zavalaleo715@yahoo.com>
2024-12-18 22:28:59 -0800

    Updated App.js, Header_Footer.css

--- src/App.js
+++ src/App.js
@@ -2,74 +2,47 @@ import logo from './Media/SHUBA.gif';
 import './App.css';
 import './Components/Header_Footer.css'
 
-function Header() {
-  return (
-    <header>
-      <div>
-        <img src="media/top_garden2_crop.png" alt="Top Garden Logo" />
-      </div>
-      <div className="logo">
-        <h1>Top Garden</h1>
-      </div>
-      <nav>
-        <ul>
-          <li><a href="#home">Home</a></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#services">Services</a></li>
-          <li><a href="#contact">Contact</a></li>
-          <li><a href="/subpage/reviews.js">Reviews</a></li>
-        </ul>
-      </nav>
-    </header>
-  );
-}
-
-function MainContent() {
-  return (
-    <main>
-      <p>What</p>
-    </main>
-  );
-}
-
-function Footer() {
-  return (
-    <footer>
-      <p>&copy; 2024 Top Garden Website. All rights reserved.</p>
-      <ul>
-        <li><a href="#privacy">Privacy Policy</a></li>
-        <li><a href="#terms">Terms of Service</a></li>
-      </ul>
-    </footer>
-  );
-}
-
 function App() {
   return (
     <div className="App">
-      <Header />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div className='Header'>
+        <header>
+          <div>
+            <img src="media/top_garden2_crop.png" alt="Top Garden Logo" />
+          </div>
+          <div className="logo">
+            <h1>Top Garden</h1>
+          </div>
+          <nav>
+            <ul>
+              <li><a href="#home">Home</a></li>
+              <li><a href="#about">About</a></li>
+              <li><a href="#services">Services</a></li>
+              <li><a href="#contact">Contact</a></li>
+              <li><a href="/subpage/reviews.js">Reviews</a></li>
+            </ul>
+          </nav>
+        </header>
+      </div>
 
-        <p>
-          <code>HELLLOOOO</code>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          What am I doing
-        </a>
-      </header>
+      <div>
+        <main>
+          <p>What</p>
+          <div>
+            <img src={logo} className="App-logo" alt="logo" />
+          </div>
+        </main>
+      </div>
 
-      <body>
-        <MainContent />
-      </body>
-
-
-      <Footer />
+      <div className='Footer'>
+        <footer>
+          <p>&copy; 2024 Top Garden Website. All rights reserved.</p>
+          <ul>
+            <li><a href="#privacy">Privacy Policy</a></li>
+            <li><a href="#terms">Terms of Service</a></li>
+          </ul>
+        </footer>
+      </div>
     </div>
   );
 }
